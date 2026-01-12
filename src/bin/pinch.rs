fb1190785e9f10b2538016f268da2a52e43c05f3
Daniel J Wilcox <danieljwilcox@gmail.com>
2026-01-12 18:03:45 +0000

    pinch args

--- src/bin/pinch.rs
+++ src/bin/pinch.rs
@@ -11,7 +11,7 @@ use std::process::Command;
     author,
     version,
     about = "pinch send youtube audio to mpd",
-    after_help = "Example:\n pinch -i 'URL'\n\nDependencies:\n yt-dlp: https://github.com/yt-dlp/yt-dlp\n  deno: https://deno.com/",
+    after_help = "Example:\n pinch -i 'URL'\n\nDependencies:\n yt-dlp: https://github.com/yt-dlp/yt-dlp\n deno: https://deno.com/",
 )]
 // disable_version_flag allows lowercase -v
 // disable_help_flag prevents the naming conflict with the manual 'help' field
