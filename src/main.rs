f44979e8f5012cd9db31e2d3dd40a055e6d49038
Daniel J Wilcox <danieljwilcox@gmail.com>
2026-01-16 20:13:13 +0000

    main

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,5 @@ fn main() {
 
     println!("\nAvailable Binaries:");
     println!("  - pinch           : pinch send youtube audio to mpd");
+    println!("  - relay-mpv       : relay a stream to a named pipe with mpv");
 }
